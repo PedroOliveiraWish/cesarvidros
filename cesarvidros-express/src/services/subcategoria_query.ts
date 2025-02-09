efdc404b99ac24ae33f4c57216a3285353e09e1b
PedroOliveiraWish <pedrowebdeveloper@gmail.com>
2025-02-09 20:12:30 -0300

    a

--- cesarvidros-express/src/services/subcategoria_query.ts
+++ cesarvidros-express/src/services/subcategoria_query.ts
@@ -1,13 +1,17 @@
 import { pool } from "../database";
 import { SubCategoria } from "../types/SubCategoria";
 
+interface SubCategoriaRequest {
+    sub_categorias: SubCategoria[];
+}
+
 const getSubCategoria = async (): Promise<SubCategoria[]> => {
     try {
         const query = `
 select categoria.id_categoria, categoria.nome as categoria_nome, json_agg(json_build_object('id_sub', sub_categoria.id_sub_categoria, 'nome', sub_categoria.nome)) as sub_categorias from categoria left join sub_categoria on categoria.id_categoria = sub_categoria.categoria_id group by categoria.id_categoria;
         `
         const subcategoria = await pool.query(query);
-        const sub = subcategoria.rows.map((item) => item.sub_categorias);
+        const sub = subcategoria.rows.map((item: SubCategoriaRequest) => item.sub_categorias);
          
         const flattenedSub = sub.flat();
         
